efd25714735bde9c1160771ce27e0c71a0d7398d
o-atsu <39590619+o-atsu@users.noreply.github.com>
2018-12-31 23:56:55 +0900

    MapControllScript_making_1231_1
    ・各データ名とデータパスを対応させるDictionary作成
    ・Refresh関数以外の作成
    *SaveAs, LoadFrom要修正

--- RANDOM_reunion/Assets/Script/Map/MapControllScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MapControllScript.cs
@@ -16,38 +16,65 @@ public class MapControllScript : MonoBehaviour, IJsonSaveLoadable, IJsonTemporar
     [DataMember]
     public Dictionary<string, MapBuildingScript> Buildings;//キー: 建物名, 値: 対応するMapObjectとする.
 
+
+    public Dictionary<string, string> Savesmap = new Dictionary<string, string>()//セーブデータ名とセーブ用ディレクトリを対応
+    {
+       { "Save1", @"Application.dataPath/Save/Save1" },
+    };
+
     //IJsonSaveLoadable
     public bool JsonExport(string path, string name, bool overwrite)//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        if (overwrite)//同名ファイルの削除
+        {
+            string absolutepath = System.IO.Path.Combine(path, name);//絶対パスの作成
+            System.IO.FileInfo(absolutepath).Delete();//作成したパスのファイル削除※ゴミ箱にいかない
+        }
+        return JsonIO.JsonExport<MapControllScript>(this, path, name);
     }
-    public bool JsonImport(string path, string name)//再帰的に適用する必要はない.
+    public MapControllScript JsonImport(string path, string name)//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        return JsonIO.JsonImport<MapControllScript>(path, name);//入力したMapControllScriptクラスのデータを返す
     }
+
     public bool SaveAs(string savename, bool overwrite)//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        return this.JsonExport(Savesmap[savename], savename, overwrite);//（マップ毎の処理が必要）
     }
-    public bool LoadFrom(string savename)//再帰的に適用する必要はない.
+    public MapControllScript LoadFrom(string savename)//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        return JsonIO.JsonImport<MapControllScript>(Savesmap[savename], savename);//(マップ毎の処理が必要)
     }
+
+
+    public Dictionary<string, string> Tempsmap = new Dictionary<string, string>()//マップ名と一時保存ディレクトリを対応
+    {
+       { "Map1", @"Application.dataPath/Temporary/Map1" },
+    };
 
     //IJsonTemporarySaveLoadable
     public bool SaveTemporary()//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        return this.JsonExport(Tempsmap[this.MapName], this.MapName, overwrite);//一時保存ディレクトリに保存
     }
-    public bool LoadTemporary()//再帰的に適用する必要はない.
+    public MapControllScript LoadTemporary()//再帰的に適用する必要はない.
     {
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        return this.JsonImport(Tempsmap[this.MapName], this.MapName);//一時保存ディレクトリからのデータを返す
     }
+
+    public Dictionary<string, path> Buildsmap = new Dictionary<string, path>()//マップ名とBuilding内のマップディレクトリを対応
+    {
+       { "Map1", @"Application.dataPath/Data/Building/Map1" },
+    };
 
     //IJsonInitializable
     public void Initialize(string mapname) //マップ名を引数にとり,対応するディレクトリからInitial.jsonを読み込み,Map,Buildingsに適用する.またMapNameも変える.
     {
-
+        MapControllScript Imported = this.JsonImport(Buildsmap[mapname], Initial);//読み込んだMapControllScriptクラスのデータをImportedに代入
+        this.MapName = mapname;//MapName適応
+        this.MapSurface = Imported.MapSurface;//MapSurface適応
+        this.Buildings = Imported.Buildings;//Buildings適応
+        return;
     }
     //IVisibleObject
     public void Refresh()//メンバもRefresh()を持っていれば再帰的に適用する.
