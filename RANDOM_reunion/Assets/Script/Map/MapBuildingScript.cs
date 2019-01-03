32bde286734c12bcd13d1ee721235a8ad831fb40
mtlilium <39237030+mtlilium@users.noreply.github.com>
2019-01-03 15:57:19 +0900

    0103_kansei1
    一応やった

--- RANDOM_reunion/Assets/Script/Map/MapBuildingScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MapBuildingScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
@@ -26,40 +27,77 @@ public class MapBuildingScript : MonoBehaviour, IJsonSaveLoadable, IJsonTemporar
     //IJsonSaveLoadable
     public bool JsonExport(string path, string name, bool overwrite)
     {
+		string FilePath = path + "/" + name + ".json";
+		if((File.Exists(FilePath) && overwrite) || !(File.Exists(path + "/" + name)))
+		{
+			JsonIO.JsonExport<MapBuildingScript> (this, path, name);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool JsonImport(string path, string name)
     {
+		string FilePath = path + "/" + name + ".json";
+		if (File.Exists (FilePath)) 
+		{
+			JsonIO.JsonImport<MapBuildingScript> (path, name);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool SaveAs(string savename, bool overwrite)
     {
+		string DirectoryPath = "Save/" + savename + "/" + Parent.MapName;
+		if (Directory.Exists (DirectoryPath) && overwrite) 
+		{
+			JsonIO.JsonExport<MapBuildingScript> (this, DirectoryPath, Parent.MapName);
+		} else {
+			Directory.CreateDirectory (DirectoryPath);
+			JsonIO.JsonExport<MapBuildingScript> (this, DirectoryPath, Parent.MapName);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool LoadFrom(string savename)
     {
+		string DirectoryPath = "Save/" + savename + "/" + Parent.MapName;
+		if (Directory.Exists (DirectoryPath)) 
+		{
+			JsonIO.JsonImport<MapBuildingScript> (DirectoryPath, Parent.MapName);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
 
     //IJsonTemporarySaveLoadable
     public bool SaveTemporary()
     {
+		string DirectoryPath = "Temporary/" + Parent.MapName;
+		if (Directory.Exists (DirectoryPath)) 
+		{
+			JsonIO.JsonExport<MapBuildingScript> (this, DirectoryPath, Parent.MapName);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool LoadTemporary()
     {
+		string DirectoryPath = "Temporary/" + Parent.MapName;
+		if(Directory.Exists (DirectoryPath))
+		{
+			JsonIO.JsonImport<MapBuildingScript> (DirectoryPath, Parent.MapName);
+		}
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
 
     //IJsonInitializable
     public void Initialize(string buildingname) //建物名を引数にとり,対応するディレクトリからInitial.jsonを読み込み,Status,ChipFieldに適用する.またbuildingnameも変える.
     {
-
+		string DirectoryPath = "Data/Building/" + Parent.MapName + buildingname;
+		MapBuildingScript tmp = JsonIO.JsonImport<MapBuildingScript> (DirectoryPath + "/Initial.json", buildingname);
+		this.BuildingName = tmp.BuildingName;
+		this.Status = tmp.Status;
+		this.ChipField = tmp.ChipField;
     }
 
     //IVisibleObject
     public void Refresh()//メンバもRefresh()を持っていれば再帰的に適用する.
     {
-
+		//ChipField.Foreach (x => x.Refresh());
     }
 }
