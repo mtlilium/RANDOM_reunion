1f3e5f62d8e95141c66520da5c3520ad45724227
hana314 <haan314@outlook.jp>
2019-01-03 15:02:58 +0900

    update_JsonExport_finishtest
    StreamWriterを使用するように変更。try-catchであらゆる例外に対してfalseを返すように。test用のオブジェクトを追加して動作確認完了

---
+++ RANDOM_reunion/Assets/JsonIO_Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+[DataContract]
+internal class Sample
+{
+    public Sample(string _name, int _age)
+    {
+        name = _name;
+        age = _age;
+    }
+    [DataMember]
+    internal string name;
+
+    [DataMember]
+    internal int age;
+}
+
+public class JsonIO_Test : MonoBehaviour {
+    
+    Sample person;
+	// Use this for initialization
+	void Start () {
+        Sample tmp = new Sample("aiueo",3);
+        JsonIO.JsonExport<Sample>(tmp, "Assets", "jsonIO_Test");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
--- RANDOM_reunion/Assets/Script/System/JsonIO.cs
+++ RANDOM_reunion/Assets/Script/System/JsonIO.cs
@@ -11,13 +11,18 @@ using UnityEngine;
 public static class JsonIO
 {
     public static bool JsonExport<T>(T obj, string path, string name)//objをpathディレクトリに[naem].jsonとして保存 成功すればtrue 失敗すればfalse
-    {
-        DataContractJsonSerializer jsonSerializer=new DataContractJsonSerializer(typeof(T));
-        MemoryStream exportResultStream=new MemoryStream();
-        jsonSerializer.WriteObject(exportResultStream, obj);
-        string exportResultString = Encoding.UTF8.GetString( exportResultStream.ToArray() );
-        File.WriteAllText(path + '/' + name + ".json", exportResultString);
-        return false;//(返り値なしだとエラーのため適当にしています.要修正)
+    {  
+        try
+        {
+            StreamWriter exportResultStream = new StreamWriter(path + '/' + name + ".json", false, Encoding.Default);
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(T));
+            jsonSerializer.WriteObject(exportResultStream.BaseStream, obj);
+        }
+        catch
+        {
+            return false;//(返り値なしだとエラーのため適当にしています.要修正)
+        }
+        return true;
     }
     public static T JsonImport<T>(string path, string name)//pathディレクトリの[naem].jsonを読み込む 失敗などで読み込めなければ,LogAssertionで警告を表示しdefault(T)を返す
     {
