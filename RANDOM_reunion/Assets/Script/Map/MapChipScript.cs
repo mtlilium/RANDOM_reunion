28cf2ee831dba838aaef42b77f794781632b1019
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2019-01-07 01:48:29 +0900

    Revert "Merge branch '1226_Kubo_MapChipScript' into 1226_Matsuda_MapBuildingScript_Modification"
    This reverts commit 392090d6cfb6e4f59cf840e17c1cf4a5195a485b, reversing
    changes made to 1b4ffd90fe25224244c9d3af3499461e45b313eb.

--- RANDOM_reunion/Assets/Script/Map/MapChipScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MapChipScript.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using UnityEngine;
-using System.IO;
 
 [DataContract]
 public class MapChipScript : MonoBehaviour , IJsonSaveLoadable ,IJsonTemporarySaveLoadable, IVisibleObject {
@@ -24,79 +23,31 @@ public class MapChipScript : MonoBehaviour , IJsonSaveLoadable ,IJsonTemporarySa
     public MapCoordinate Coordinate{get;set;}//MapCoordinateでの座標
     
     //IJsonSaveLoadable
-    //ただのスラッシュでいいのかバックスラッシュじゃないのか
     public bool JsonExport(string path, string name, bool overwrite)
     {
-        if((File.Exists(path+"/"+name)&& overwrite)||!(File.Exists(path + "/" + name)))
-        {
-            if (JsonIO.JsonExport<MapChipScript>(this, path, name))
-            {
-                return true;
-            }
-        }
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool JsonImport(string path, string name)
     {
-        if (File.Exists(path + "/" + name)&&JsonIO.JsonImport<MapChipScript>(path, name))
-        {
-           return true;
-        }
         return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool SaveAs(string savename, bool overwrite)
     {
-        if (JsonIO.JsonExport<MapChipScript>(this, "Save/" + savename + "/" + Parent.Parent.MapName, savename)&&overwrite)
-        {
-            return true;
-        }
-        else
-        {
-            Debug.LogAssertion("せーぶできませんでした");
-            return false;
-        }
-        //(返り値なしだとエラーのため適当にしています.要修正)
+        return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool LoadFrom(string savename)
     {
-        if(JsonIO.JsonImport<MapChipScript>("Save/" + savename + "/" + Parent.Parent.MapName, savename))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        //Parent.BuildingNameかも
-        //(返り値なしだとエラーのため適当にしています.要修正)
+        return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
 
     //IJsonTemporarySaveLoadable
     public bool SaveTemporary()
-    {   
-        if(JsonIO.JsonExport<MapChipScript>(this, "Temporary/" + Parent.Parent.MapName, Parent.Parent.MapName))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        //(返り値なしだとエラーのため適当にしています.要修正)
+    {
+        return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
     public bool LoadTemporary()
     {
-        if (File.Exists("Temporary/"+ Parent.Parent.MapName+"/"+ Parent.Parent.MapName))
-        {
-            JsonIO.JsonImport<MapChipScript>("Temporary/" + Parent.Parent.MapName, Parent.Parent.MapName);
-            return true;
-        }
-        else
-        {
-            Debug.LogAssertion("てんぽらりーせーぶできませんでした");
-            return false;
-        }
-        //(返り値なしだとエラーのため適当にしています.要修正)
+        return false;//(返り値なしだとエラーのため適当にしています.要修正)
     }
 
     //IVisibleObject
