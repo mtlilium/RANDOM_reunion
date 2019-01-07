f054ecc0e166a5771cd0ca0997f50588d5c0cc86
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-01-07 22:47:03 +0900

    MovableObjectScript_making_0106
    ・Move関数の実装
    ※MapCoordinate関数との兼ね合いまだ

---
+++ RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+public class MovableObjectScript : MonoBehaviour {
+    Rigidbody2D rigid2D;
+    Vector2 from = rigid2D.transform.position;
+
+    public void Move(Vector2 to, MapCoordinate mapcoodinate)
+    {
+        Vector2.Leap(this.from, from + to, 1);
+        mapcoodinate.x += to.x;
+        mapcoodinate.y += to.y;
+        return;
+    }
+	
+}
