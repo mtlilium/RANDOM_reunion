0a2be6dc2aa77db20e1fe0dd703b8065f4da0973
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-01-08 23:29:20 +0900

    MovableObjectScript_making_0108_2
    ・コメントの追加
    ※回転の実装は別としている

--- RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
@@ -6,8 +6,8 @@ using System.Runtime.Serialization.Json;
 using UnityEngine;
 
 public class MovableObjectScript : MonoBehaviour {
-    Rigidbody2D rigid2D = GetComponent<RigidBody2D>();
-    int rbx = rigid2D.position.x;
+    Rigidbody2D rigid2D = GetComponent<RigidBody2D>();//RigidBody2Dの取得
+    int rbx = rigid2D.position.x;//現在地をrbx, rbyとする
     int rby = rigid2D.position.y;
     Vector2 from = new Vector2(
         rbx,
@@ -16,8 +16,8 @@ public class MovableObjectScript : MonoBehaviour {
 
     public void Move(Vector2 to, MapCoordinate mapcoordinate)
     {
-        rigid2D.MovePosition(from + to);
-        mapcoordinate.x = -MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;
+        rigid2D.MovePosition(from + to);//現在地からtoの方向に進む
+        mapcoordinate.x = -MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;//MapCoordinateのx,yの更新
         mapcoordinate.y = MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;
         return;
     }
