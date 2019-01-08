ef9903bc05d0826f035e668b6d7e64cc29af5f58
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-01-08 11:47:26 +0900

    MovableObjectScript_making_0108_1
    ・Mapcoordinate関数内のx,yかきかえを実装
    ※型はintで大丈夫なのか

--- RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
@@ -6,22 +6,21 @@ using System.Runtime.Serialization.Json;
 using UnityEngine;
 
 public class MovableObjectScript : MonoBehaviour {
-    Rigidbody2D rigid2D;
-    public static Vector2 from = rigid2D.transform.position;
+    Rigidbody2D rigid2D = GetComponent<RigidBody2D>();
+    int rbx = rigid2D.position.x;
+    int rby = rigid2D.position.y;
+    Vector2 from = new Vector2(
+        rbx,
+        rby
+    );
 
-    public void Move(Vector2Int to)
+    public void Move(Vector2 to, MapCoordinate mapcoordinate)
     {
-        Vector2.Lerp(this.from, from + to, 1);
-        
+        rigid2D.MovePosition(from + to);
+        mapcoordinate.x = -MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;
+        mapcoordinate.y = MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;
         return;
     }
-    public override Vector2 ToVector2()
-    {//Vector2に変換
-        return new Vector2(
-        (-x + y) / 2f / MapChipScript.CHIP_WIDTH,
-        (x + y) / 2f / MapChipScript.CHIP_HEIGHT
-     );
-    }
-
+    
 
 }
