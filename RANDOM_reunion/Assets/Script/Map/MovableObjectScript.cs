d046280342dca0ea454f6f6a912d792f74a18849
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-01-10 12:13:08 +0900

    MovableObjectScript_modify_0110
    Rigidbody2Dの修正（未）

--- RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
+++ RANDOM_reunion/Assets/Script/Map/MovableObjectScript.cs
@@ -6,21 +6,26 @@ using System.Runtime.Serialization.Json;
 using UnityEngine;
 
 public class MovableObjectScript : MonoBehaviour {
-    Rigidbody2D rigid2D = GetComponent<RigidBody2D>();//RigidBody2Dの取得
-    int rbx = rigid2D.position.x;//現在地をrbx, rbyとする
-    int rby = rigid2D.position.y;
+
+    public Rigidbody2D rigid2D { get; set; }
     Vector2 from = new Vector2(
-        rbx,
-        rby
+        transform.position.x,
+        transform.position.y
     );
 
-    public void Move(Vector2 to, MapCoordinate mapcoordinate)
+    
+    public void Move(Vector2 to)
     {
-        rigid2D.MovePosition(from + to);//現在地からtoの方向に進む
-        mapcoordinate.x = -MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;//MapCoordinateのx,yの更新
-        mapcoordinate.y = MapChipScript.CHIP_WIDTH * rbx + MapChipScript.CHIP_HEIGHT * rby;
+        this.GetComponent<Rigidbody2D>().MovePosition(from + to);//現在地からtoの方向に進む
+        
         return;
     }
-    
 
+    /*mapcoordinate.x = -MapChipScript.CHIP_WIDTH* rbx + MapChipScript.CHIP_HEIGHT* rby;//MapCoordinateのx,yの更新
+    mapcoordinate.y = MapChipScript.CHIP_WIDTH* rbx + MapChipScript.CHIP_HEIGHT* rby;*/
+
+    public void Move(MapCoordinate mapcoordinate)
+    {
+
+    }
 }
