086c9ccc67630db7e23a623d0bfbd88437680a28
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2019-02-09 18:14:31 +0900

    _Interface_Modification
    IJsonIOをIJsonIとIJsonOに分離,並びに関連するインターフェースを書き換え

--- RANDOM_reunion/Assets/Script/System/_Interfaces.cs
+++ RANDOM_reunion/Assets/Script/System/_Interfaces.cs
@@ -1,15 +1,23 @@
 
-interface IJsonIO{//.json形式での入出力をサポートするものに付与するインターフェース. 実際にはUtility.JsonExport<T>とUtility.JsonImport<T>を使う
-    bool JsonExport(string path,string name,bool overwrite);//pathディレクトリに[naem].jsonとして保存 成功すればtrue 失敗すればfalse
-    bool JsonImport(string path,string name);//pathディレクトリの[naem].jsonを読み込み,自分自身に適用 成功すればtrue 失敗すればfalse
+interface IJsonI
+{//.json形式での入力をサポートするものに付与するインターフェース. 実際にはUtility.JsonImport<T>を使う
+    bool JsonImport(string path, string name);//pathディレクトリの[naem].jsonを読み込み,自分自身に適用 成功すればtrue 失敗すればfalse
 }
 
-interface IJsonSavable : IJsonIO
+interface IJsonO
+{//.json形式での出力をサポートするものに付与するインターフェース. 実際にはUtility.JsonExport<T>を使う
+    bool JsonExport(string path, string name, bool overwrite);//pathディレクトリに[naem].jsonとして保存 成功すればtrue 失敗すればfalse
+}
+
+interface IJsonIO : IJsonI, IJsonO
+{/*.json形式での入出力をサポートするものに付与するインターフェース.*/}
+
+interface IJsonSavable : IJsonO
 {//.json形式でセーブするものに付与するインターフェース.
     bool SaveAs(string savename, bool overwrite);//セーブ名をsavenameとしてデータを出力. overwriteで上書きの許可,不許可を制御. 成功すればtrue 失敗すればLogAssertionで警告を表示しfalse
 }
 
-interface IJsonLoadable : IJsonIO
+interface IJsonLoadable : IJsonI
 {//.json形式でロードするものに付与するインターフェース.
     bool LoadFrom(string savename);//セーブ名savenameからデータを入力. overwriteで上書きの許可,不許可を制御. 成功すればtrue データが存在しないなどで失敗すればLogAssertionで警告を表示しfalse
 }
@@ -17,12 +25,12 @@ interface IJsonLoadable : IJsonIO
 interface IJsonSaveLoadable : IJsonSavable, IJsonLoadable
 {/*.json形式でセーブ,ロードするものに付与するインターフェース.*/}
 
-interface IJsonTemporarySavable : IJsonIO
+interface IJsonTemporarySavable : IJsonO
 {//.json形式でセーブするものに付与するインターフェース.
     bool SaveTemporary();//Temporaryにデータを出力.既に存在すれば上書き. 成功すればtrue 失敗すればLogAssertionで警告を表示しfalse
 }
 
-interface IJsonTemporaryLoadable : IJsonIO
+interface IJsonTemporaryLoadable : IJsonI
 {//.json形式でロードするものに付与するインターフェース.
     bool LoadTemporary();//Temporaryからデータを入力. 成功すればtrue データが存在しないなどで失敗すればLogAssertionで警告を表示しfalse
 }
@@ -30,7 +38,7 @@ interface IJsonTemporaryLoadable : IJsonIO
 interface IJsonTemporarySaveLoadable : IJsonTemporarySavable, IJsonTemporaryLoadable
 {/*.json形式で一時的にセーブ,ロードするものに付与するインターフェース.*/}
 
-interface IJsonInitializable : IJsonIO
+interface IJsonInitializable : IJsonI
 {//初期にロードするデータ(Initial.json)を持っているものに付与するインターフェース.
     void Initialize(string name);//nameのInitial.jsonのロード
 }
