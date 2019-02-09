d7d6d6021ed699e0fddd0d0380a9b424470b6aa2
mtlilium <39237030+mtlilium@users.noreply.github.com>
2019-02-09 13:52:56 +0900

    dekita

---
+++ RANDOM_reunion/Assets/Script/Map/PlayerOperateScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+
+public class PlayerOperateScript : MovableObjectScript {
+
+	[DataMember]
+	Vector2 tmp;
+
+	void Start(){
+		tmp = new Vector2 (0,0);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (IsInput()) {
+			Move (tmp);
+		}
+	}
+
+
+	private bool IsInput(){
+		tmp = new Vector2 (0,0);
+		if (!Input.anyKeyDown) {
+			return false;
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			tmp += new Vector2 (1, 1);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			tmp += new Vector2 (-1, -1);
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			tmp += new Vector2 (1, 0);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			tmp += new Vector2 (0, 1);
+		} 
+		return true;
+	}
+
+}
