e389ada820e77167eb39720249ae87cc2e58092f
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-03-15 07:21:48 +0900

    0314_ItemInfo_making
    変数、関数名だけ作った
    ※関数の実装まだ

---
+++ RANDOM_reunion/Assets/Script/System/ItemInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+[DataContract]
+public static class ItemInfo {
+    List<ItemProperty> ItemField;
+
+    [DataMember]
+    Dictionary<string, int> ItemIDResolution;
+    [DataMember]
+    List<string> ItemNameResolution;
+
+    public bool HaveAttribute(string itemname, string attribute)
+    {
+
+    }
+    public bool HaveAttribute(int itemid, string attribute)
+    {
+
+    }
+    public void LoadItemInfo()
+    {
+
+    }
+	
+}
