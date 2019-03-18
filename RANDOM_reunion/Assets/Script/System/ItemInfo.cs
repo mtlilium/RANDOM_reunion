b815ce24ef1e46ef9e801be24e0876b20b7c18fa
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-03-19 00:11:57 +0900

    0318_ItemInfo_making
    LoadItemInfoの修正
    ※IDについて規定があるなら要変更

--- RANDOM_reunion/Assets/Script/System/ItemInfo.cs
+++ RANDOM_reunion/Assets/Script/System/ItemInfo.cs
@@ -8,13 +8,17 @@ using UnityEngine;
 [DataContract]
 public static class ItemInfo {
 
-    static List<ItemProperty> ItemField;//それぞれのアイテムがItemID番目に保存されると仮定した
+    /*IDについて、スクリプトを読み込んだ順にIDも（0から）決まると仮定した*/
+
+    static List<ItemProperty> ItemField;
     [DataMember]
     static Dictionary<string, int> ItemIDResolution;
     [DataMember]
     static List<string> ItemNameResolution;
 
-    public static bool HaveAttribute(string itemname, string attribute)//対象のアイテムのAttributeにattributeがあればtrue, そうでなければfalse
+    static ItemProperty itemProperty;
+
+    public static bool HaveAttribute(string itemname, string attribute)//対象のアイテムのAttributeリストにattributeがあればtrue, そうでなければfalse
     {
         int thisItemID = ItemIDResolution[itemname];
         LinkedList<string> thisAttribute = ItemField[thisItemID].Attribute;
@@ -28,7 +32,7 @@ public static class ItemInfo {
         }
         return false;
     }
-    public static bool HaveAttribute(int itemid, string attribute)//対象のアイテムのAttributeにattributeがあればtrue, そうでなければfalse
+    public static bool HaveAttribute(int itemid, string attribute)//対象のアイテムのAttributeリストにattributeがあればtrue, そうでなければfalse
     {
         LinkedList<string> thisAttribute = ItemField[itemid].Attribute;
 
@@ -41,23 +45,25 @@ public static class ItemInfo {
         }
         return false;
     }
-    public static void LoadItemInfo()//メンバ変数の更新
+    public static void LoadItemInfo()//メンバ変数とItemFieldの更新
     {
         string path = SystemVariables.RootPath + "/Data/Item/ItemProperty";
-        int idtemp = 0;
-        ItemProperty itemProperty;
-
         string[] files = System.IO.Directory.GetFiles(@path, "*", System.IO.SearchOption.TopDirectoryOnly);
-        foreach(string i in files)
+
+        ItemField = new List<ItemProperty>();//メンバ変数とItemFieldを空に
+        ItemIDResolution = new Dictionary<string, int>();
+        ItemNameResolution = new List<string>();
+
+        foreach(string i in files)//全アイテムを読み込み, ItemField, ItemIDResolution, ItemNameResolutionに追加
         {
             string name = System.IO.Path.GetFileNameWithoutExtension(i);
             if (itemProperty.JsonImport(path, name))
             {
-                ItemField.
+                int idtemp = ItemIDResolution.Count;
+                ItemField.Add(itemProperty);
+                ItemIDResolution.Add(itemProperty.Name, idtemp);//末尾に追加しIDは0からはじめ、追加前の長さをIDとした
+                ItemNameResolution.Add(itemProperty.Name);
             }
         }
-
-
     }
-	
 }
