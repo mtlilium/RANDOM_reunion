9236f4f1baefe8f2473aced257ea3bdaea7df455
machi7 <45916369+machi7@users.noreply.github.com>
2019-05-06 14:58:01 +0900

    FlagsOK
    FlagsForSerialization classわからん

---
+++ RANDOM_reunion/Assets/Script/System/Flags.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Flags /*:MonoBehaviour*/ {
+
+    public static Dictionary<string, bool> FlagField;
+
+    public static bool GetFlag(string flagname)
+    {
+        if (FlagField.ContainsKey(flagname))
+        {
+            return FlagField[flagname];
+        }
+        else
+        {
+            Debug.Log(flagname + "というキーは存在しません");
+            return false;
+        }
+    }
+    public static bool HaveFlag(string flagname)
+    {
+        if (FlagField.ContainsKey(flagname))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
